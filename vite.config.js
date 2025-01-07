9d8c68684ca93091216773c2d192408db3e73070
Samuele Scatena <samuele@mac-mini-black>
2025-01-07 17:28:04 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: { port: 9000 },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
